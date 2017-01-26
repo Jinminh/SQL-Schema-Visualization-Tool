d3143f257c8e33cb63009aff956ef94f650e0dee
jinminh93 <jinminh93@gmail.com>
2017-01-26 16:30:37 -0500

    finished mysql selection

--- test.js
+++ test.js
@@ -7,8 +7,34 @@ var app = express();
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-function val(someval){
-  return someval;
+function removeDuplicates(arr) {
+     var new_arr = [];
+     var lookup  = {};
+  
+     new_arr.push(arr[0]);
+     var first_key = Object.keys(new_arr[0])[0];
+     var arr_first_key;
+     var new_arr_first_key;
+
+     //console.log('new>>>>'+Object.keys(new_arr[0])[0]);
+     
+     for(var i=1; i<arr.length;i++){
+       var found = 0;     
+       arr_first_key = Object.keys(arr[i])[0];
+       for(var j=0; j<new_arr.length;j++){
+         new_arr_first_key = Object.keys(new_arr[j])[0]
+         if(arr_first_key == new_arr_first_key){
+           found = 1;
+           break;
+         }
+       }
+       if(found === 0){
+         new_arr.push(arr[i]);
+       }
+     }
+     return new_arr;
+     
+    
 }
 // on load get connections from local file
 
@@ -37,8 +63,6 @@ app.get('/process_get',function(req,res){
     port:response.pn
   });
   
-  var result;
-  
   //create connection
   conn.connect(function(err) {
     if(err){
@@ -56,9 +80,13 @@ app.get('/process_get',function(req,res){
 
     console.log("The file was saved!");
 });
+  
+  
+var jsonObj = [];
+    
   //search table names first 
   conn.query('SELECT TABLE_NAME FROM TABLES Where table_schema =?', [response.db],function(err,tables,fields){
-    var jsonObj = [];
+    
     
     console.log('connected');
     console.log('\n');
@@ -66,54 +94,105 @@ app.get('/process_get',function(req,res){
       return res.send(JSON.stringify(err));
     } 
    
+    
+    /*Since the mysql query is a call back function
+    All the variable below is for counting and make sure that 
+    the program will execute the res.end() after all the queries finished*/
+    
+    //count the number of tables in the databse 
     var count = 0;
+    
+    //count the times of select contraints query execute
+    var cnt = 0;    
+    //the total number of columns in the database
+    var col_cnt = 0;
+    //count the column number in each table
+    var temp_num = 0;
     for (var i in tables) {    
-        
         //according to table names, select columns for each table
-        conn.query('SELECT COLUMN_NAME FROM `COLUMNS` WHERE TABLE_NAME = ?',[tables[i].TABLE_NAME], function(err, cols, fields){
+        conn.query('SELECT COLUMN_NAME, DATA_TYPE FROM `COLUMNS` WHERE TABLE_NAME = ?',[tables[i].TABLE_NAME], function(err, cols, fields){
           if(err){
             return res.send(JSON.stringify(err));
           }
           
+          /*create items for jsonObj
+          the format for each item is
+          {
+            "table":"table_name"
+            "col0":{"column_name":"name", 
+            "column_type":"type",
+            "constrain_name":"con_name",
+            "referenced_table_name":"ref_t_name",
+            "referenced_column_name":"ref_col_name"}
+            "col1": ...
+            "col2": ...
+            ...
+           }
+           */
           var item = {};
-          item[' table'+count] = tables[count].TABLE_NAME;
+          item['table'+count] = tables[count].TABLE_NAME;
           console.log('table'+count +':', tables[count].TABLE_NAME);
           
-          for(var j in cols){
-            console.log('cols'+j +':', cols[j].COLUMN_NAME);
-            
-//             conn.query('SELECT CONSTRAINT_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM `KEY_COLUMN_USAGE` WHERE TABLE_NAME=? AND COLUMN_NAME=?'
-//                       , [tables[count].TABLE_NAME, cols[j].COLUMN_NAME], function(err, constraints, fields){
-//               if(err){
-//                 return res.send(JSON.stringify(err));
-//               }
-              
-//               console.log('constraints'+':', cols[j], constraints[0].CONSTRAINT_NAME, constraints[0].REFERENCED_TABLE_NAME, constraints[0].REFERENCED_COLUMN_NAME);
-//               console.log('\n');  
-              
-//             });
-            
-            
-            item['col'+j] = cols[j].COLUMN_NAME;
+          //count the total column number in the database
+          for(var temp in cols){
+            col_cnt++;
           }
           
-          count++;
-          console.log('\n');
-          jsonObj.push(item);
-          if (count == Object.keys(tables).length){
-           //res.render("process_get")
-            //res.sendFile(__dirname+'\\public\\process_get.html');
-            res.end(JSON.stringify(jsonObj));
+          for(var j in cols){     
+            
+            //insert name and type to the column
+            item['col'+j] = {name:cols[j].COLUMN_NAME, type:cols[j].DATA_TYPE}; 
+            
+            //according to the column names, select if there is any contraints for each column
+            conn.query('SELECT TABLE_NAME, COLUMN_NAME, CONSTRAINT_NAME, REFERENCED_TABLE_NAME, REFERENCED_COLUMN_NAME FROM `KEY_COLUMN_USAGE` WHERE TABLE_NAME=? AND COLUMN_NAME=?'
+                      , [item['table'+count], cols[j].COLUMN_NAME], function(err, constraints, fields){
+
+               if(err){
+                 return res.send(JSON.stringify(err));
+               }
+
+              //if there is a constraints, add it to the column attributes
+              if(typeof constraints[0] != 'undefined'){         
+                console.log('constraints'+':', constraints[0].TABLE_NAME, constraints[0].COLUMN_NAME, constraints[0].CONSTRAINT_NAME, constraints[0].REFERENCED_TABLE_NAME, constraints[0].REFERENCED_COLUMN_NAME);
+                console.log('temp_num'+temp_num);
+                console.log(item['col'+temp_num]);
+                
+                //insert constraint attributes to the column
+                item['col'+temp_num]['constrain_name'] = constraints[0].CONSTRAINT_NAME;
+                item['col'+temp_num]['referenced_table_name'] = constraints[0].REFERENCED_TABLE_NAME;
+                item['col'+temp_num]['referenced_column_name'] = constraints[0].REFERENCED_COLUMN_NAME
+                jsonObj.push(item);
+              }else{
+                jsonObj.push(item);
+              }
+              
+//               console.log('temp_numhrererere>>>'+temp_num);
+//               console.log('col<>>>>>'+Object.keys(cols).length);
+              
+              //reset the count of column number after finishing a table
+              if(temp_num == Object.keys(cols).length-1){
+                temp_num= -1;
+              }     
+              temp_num++;
+              console.log('\n')
+              cnt++;
+              //console.log('cnt>>>'+cnt);
+              
+              //if all columns have been selected, res.send the jsonObj
+               if(cnt == col_cnt){
+                  var final_obj = removeDuplicates(jsonObj);
+                  console.log('res.send!!!');
+                  res.end(JSON.stringify(final_obj));
+                }
+            });           
           }
+          count++;
         });
     }
   });
   
-  //res.end('connected');
-  //conn.end();
 })
-        
-       
+              
 
 
 var server = app.listen(7474, function(){

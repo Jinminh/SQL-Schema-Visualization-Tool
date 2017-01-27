426482d6b5554c0d116849ba067d5e6941751519
Caaraya <christina-araya@hotmail.com>
2017-01-27 10:27:49 -0800

    added tables to list, pulled data and ordered it

--- public/app.js
+++ public/app.js
@@ -1,20 +1,43 @@
 
-
-// // function connection(){
-  
-// // }
- window.onload = function(){
-         // xmlhttprequest - get data from my connection endpoint or something
-         // in the response make use of the data we've asked for
-         var requestTables = new XMLHttpRequest()
-         requestTables.onreadystatechange = function()
+var tables
+var displayedTable
+ function loadList(tableobj)
+ {
+     var list = document.getElementById("myTableList")
+     tables = {};
+     for(i=0; i<tableobj.length; i++)
+     {
+         var item = document.createElement('li')
+         item.appendChild(document.createTextNode(tableobj[i]["table"+i]))
+         item.addEventListener("click", addTable, false);
+         var obj = [];
+         for(j=0; j<Object.keys(tableobj[i]).length-1; j++)
          {
-             if (requestTables.readyState == 4)
-             {
-                 var details = "DONE HERE ARE MY DETAIL <br/>" + requestTables.responseText
-                 document.getElementById("myDiagramDiv").innerHTML = details
-             }
+             obj[j] = tableobj[i]['col'+j]
          }
-         requestTables.open('GET', 'http://localhost:7474/tabledata', true)
-         requestTables.send(null)
+         tables[tableobj[i]["table"+i]] = obj
+         list.appendChild(item)
+     }
+     
  }
+ 
+ function addTable(event) {
+    var dataValue = event.target.firstChild.nodeValue; // value of TextNode created by elements.createTextNode(Data)
+    // handle dataValue
+}
+ window.onload = function(){
+    // xmlhttprequest - get data from my connection endpoint or something
+    // in the response make use of the data we've asked for
+    var requestTables = new XMLHttpRequest()
+    requestTables.onreadystatechange = function()
+    {
+        if (requestTables.readyState == 4)
+        {
+            loadList(JSON.parse(requestTables.responseText))
+        }
+    }
+    requestTables.open('GET', 'http://localhost:7474/tabledata', true)
+    requestTables.send(null)
+ }
+ 
+
